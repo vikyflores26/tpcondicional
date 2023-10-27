e3152521a6714c258da383d9b48533b16a787524
alumno <alumno@Instituto.local>
2023-10-27 09:28:39 -0300

    cambios

---
+++ condicional.js
@@ -0,0 +1,22 @@
+if (( nombre == 'santino')) || ((nombre  == 'nahuel '))
+{
+    alert ( ' bienvenido')
+}else{
+    alert ( 'bienvenido, ')
+
+}
+
+if (edad >= 18 )
+{ else}
+alert ( "sos menos de edad")
+}
+
+if (edad <= 11 ){
+    alert ('sos un niño')
+}else if (edad < = 18 ){
+    else if( edad <= 26 ){
+        alert ('joven - adulto')
+    }else if ( edad <= 59 ){
+alert ( 'adulto')
+}
+
